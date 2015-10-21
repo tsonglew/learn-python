69933932e87308c7f77c5b606bd4930f9c6dbd8e
Kasheem Lew <kasheem_lew@outlook.com>
2015-10-21 10:39:05 +0800

    Updated with Chinese&some annotations

--- kick_the_ball.py
+++ kick_the_ball.py
@@ -1,17 +1,23 @@
-from random import choice
+# coding: utf-8
+# kick the ball,the game.py
+
+from random import choice  # make a random choice from the list of the direction below   
+ 
+
+direction = ['左边','右边','中间']  # make a list of dirctions
+print "轮到你点球了，准备射向哪边？从下面几个中选择一个吧！"
+print "左边,中间,右边"
 
 
-print "Choose one side bellow to shoot!"
-print "left,center,right"
-your_choice = raw_input(">")
+your_choice = raw_input(">")  # assign what you choose to your_choice
+print "你射向了 %s."%your_choice
 
-print "You kicked %s."%your_choice
-direction = ['left','center','right']
-com = choice(direction)
 
-print 'Computer saved %s.'%com
+com = choice(direction)  # computer makes the random choice
+print "守门员扑向了 %s."%com
+
 
 if your_choice != com:
-    print "GOALLLLLLLL!!!!!!!"
+    print "进了!"
 else:
-    print "Oops..."
+    print "可惜."  # print the consequense
