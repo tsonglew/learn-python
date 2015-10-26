934c5c00b65b2f893c118f8af48f42a6871d7996
kasheemlew <417879751@qq.com>
2015-10-26 14:09:13 +0800

    ltd

---
+++ ltd.py
@@ -0,0 +1,37 @@
+# coding: utf-8
+
+
+aList = [1, 2, 3, 4]  #to establish a list
+
+
+print aList
+print aList[:2]
+print aList[2:]
+print alist[2]    #to print the element you want in the list
+
+
+aList[1] = 5   #to make a little bit change in the list
+
+
+print aList #print the new list
+
+
+aTuple = ('list', 'tuple', 'dict', 'robot') #to form a tuple
+
+
+print aTuple
+print aTuple[:2]
+print aTuple[2:]
+print aTuple[2]   #print the tuple.A tuple is unable to be changed
+
+
+aDict = {1:'list', 2:'tuple', 3:'dict'} #form an dict
+
+
+print aDict
+
+
+aDict[4] = keyword #add a factor to the dict
+
+
+print aDict
