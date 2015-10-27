bd575249d90e08ede176413fbbd3697624b71ed0
Kasheem Lew <kasheem_lew@outlook.com>
2015-10-27 14:18:10 +0800

    Update makeTextFile.py

--- makeTextFile.py
+++ makeTextFile.py
@@ -4,19 +4,14 @@
 'makeTextFile.py -- create text file'
 
 
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
 import os
 ls = os.linesep
 
 
 # get filename
 while True:
-
-
     if os.path.exists(fname):
-        print "ERROR: '%s' already exists" % fname
+        print "ERROR: '%s' already exists"%fname
     else:
         break
 
