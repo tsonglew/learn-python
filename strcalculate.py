8dc9d10f730e035a8e1c735f754aa6aa1e54d856
kasheemlew <417879751@qq.com>
2015-11-16 13:56:44 +0800

    151116

---
+++ strcalculate.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+
+import os
+# the alphabet
+alet = 'abcdefghijklmnopqrstuvwxyz'
+
+
+# establish a dict
+aDict = {}
+
+
+# assign the characters in the alphabet
+n = 1
+for i in alet:
+    aDict[i] = n
+    n += 1
+
+
+# input the str into a text
+file = 'a.txt'
+fobj = open(file, 'w')
+
+
+while True:
+    aLine = raw_input('Enter a line("." to quit): ').lower()
+    if aLine != '.':
+        fobj.write('%s%s' % (aLine, os.linesep))
+    else:
+        break
+
+
+fobj.close()
+
+
+# check the text by line and change the into numericial values
+f = open(file, 'r')
+for eachLine in f:
+    m = 0
+    for i in eachLine:
+        if i in alet:
+            m += aDict[i]
+        else:
+            pass
+    print m
+f.close()
