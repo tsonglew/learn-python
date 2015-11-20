fd37189cbe49b477b96d94152798ef59f9188efe
Kasheem Lew <417879751@qq.com>
2015-11-20 14:18:12 +0800

    Delete exercise13.py

--- exercise13.py
+++
@@ -1,8 +0,0 @@
-from sys import argv
-
-script,first,second,third=argv
-
-print"The script is called:",script
-print"Your first variable is:",first=raw_input()
-print"Your second variable is:",second=raw_input()
-print"Your third variable is:",third=raw_input()
