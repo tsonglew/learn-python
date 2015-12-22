333df9ed160e685bb4d6348489132cbfb889b0d4
kasheemlew <417879751@qq.com>
2015-12-22 14:28:48 +0800

    151222

--- flask/flaskr_tests.py
+++ flask/flaskr_tests.py
@@ -16,6 +16,52 @@ class FlaskrTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(flaskr.app.config['DATABASE'])
 
+    def test_empty_db(self):
+        rv = self.app.get('/')
+        assert 'No entries here so far' in rv.data
+
+    def login(self, username, password):
+        return self.app.post('/login', data = dict(
+            username = username,
+            password = password
+        ), follow_refirects = True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects = True)
+
+    # make sure login and logout works
+    def test_login_logout(self):
+        rv = self.login('admin', 'default')
+        assert 'You were logged in' in rv.data
+        rv = self.logout()
+        assert 'You were logged out' in rv.data
+        rv = self.login('adminx', 'default')
+        assert 'Invalid username' in rv.data
+        rv = self.login('admin', 'defaultx')
+        assert 'Invalid password' in rv.data
+
+    # check that HTML is allowed in the text but not in the title
+    def test_messages(self):
+        self.login('admin', 'default')
+        rv = self.app.post('/add', data = dict(
+            title = '<Hello>',
+            text = '<strong>HTML</strong> allowed here'
+        ), follow_redirects = True)
+        assert 'No entries here so far' not in rv.data
+        assert '&lt;Hello&gt;' in rv.data
+        assert '<strong>HTML</strong> allowed here' in rv.data
+
+'~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
+'======================OTHER=TESING=TRICKS============================='
+
+app = falsk.Flask(__name__)
+
+# activate a request context temporarily
+#with app.test_request_context('/?name = Peter'):
+#    assert flask.request.path == '/'
+#    assert flask.request.args['name'] == 'Peter'
+
+
 
 if __name__ == '__main__':
     unittest.main()
