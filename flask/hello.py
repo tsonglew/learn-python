69392f2f17cb5be80378211e332ba742c969b5ae
kasheemlew <417879751@qq.com>
2016-01-02 19:57:00 +0800

    160102

--- flask/hello.py
+++ flask/hello.py
@@ -19,7 +19,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
