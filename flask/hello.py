0afeee417a81eec0ec45c98169f78848c315bc0f
kasheemlew <417879751@qq.com>
2016-01-03 00:12:06 +0800

    160103

--- flask/hello.py
+++ flask/hello.py
@@ -33,3 +33,22 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('index'))
+    return render_template('index.html',
+            form=form, name=session.get('name'),
+            known=session.get('known', False))
