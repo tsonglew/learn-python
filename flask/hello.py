b862275c172e0fe1aaae158e8135a9b2b98886b5
kasheemlew <417879751@qq.com>
2016-01-06 13:17:11 +0800

    160106

--- flask/hello.py
+++ flask/hello.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import shell
+from threading import Thread
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -57,3 +58,12 @@ def index():
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 manager.add_command("shell", Shell(make_context=make_shell_context))
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+            sender=app.config['FLASKY_MAIL_SENDER'], recipient=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app,msg])
+    thr.start()
+    return thr
