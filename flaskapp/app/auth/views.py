3dba425cf0b8e0579b49f6b2adae6652eb0fc210
kasheemlew <417879751@qq.com>
2016-01-16 09:05:58 +0800

    160116

--- flaskapp/app/auth/views.py
+++ flaskapp/app/auth/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, request, url_for, flash
+from flask import render_template, redirect, request, url_for, flash, g
 from flask.ext.login import login_user, logout_user, login_required, \
         current_user
 from . import auth
@@ -10,11 +10,13 @@ from .forms import LoginForm, RegistrationForm
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+    g.current_user = ""
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is not None and user.verify_password(form.password.data):
             login_user(user, form.remember_me.data)
+            g.current_user = user
             return redirect(request.args.get('next') or url_for('main.index'))
         flash('Invalid username or password')
     return render_template('auth/login.html', form=form)
@@ -26,6 +28,7 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
 
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
--- flaskapp/app/models.py
+++ flaskapp/app/models.py
@@ -13,7 +13,7 @@ class Role(db.Model):
         return '<Role %r>' % self.name
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
