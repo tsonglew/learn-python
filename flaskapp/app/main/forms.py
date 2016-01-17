d1f9c9173105ad0a6afbf2e794b62bc338f03127
kasheemlew <417879751@qq.com>
2016-01-17 23:20:58 +0800

    160117

--- flaskapp/app/main/forms.py
+++ flaskapp/app/main/forms.py
@@ -2,6 +2,7 @@ from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField, TextAreaField, BooleanField, \
                     SelectField, ValidationError
 from wtforms.validators import Required, Length, Email, Regexp
+from ..models import Role, User
 
 
 class NameForm(Form):
@@ -45,3 +46,8 @@ class EditProfileAdminForm(Form):
         if field.data != self.user.username and \
                 User.query.filter_by(username=field.data).first():
             raise ValidationError('Username already in user.')
+
+
+class PostForm(Form):
+    body = TextAreaField("What's on your mind?", validators=[Required()])
+    submit = SubmitField('Submit')
--- flaskapp/app/main/views.py
+++ flaskapp/app/main/views.py
@@ -3,9 +3,9 @@ from flask import render_template, session, redirect, url_for, current_app, \
 from flask.ext.login import login_required, current_user
 from datetime import datetime
 from . import main
-from .forms import NameForm, EditProfileForm, EditProfileAdminForm
+from .forms import NameForm, EditProfileForm, EditProfileAdminForm, PostForm
 from .. import db
-from ..models import Role, User
+from ..models import Role, User, Permission, Post
 from ..email import send_email
 from ..decorators import admin_required
 
@@ -13,22 +13,17 @@ from ..decorators import admin_required
 @main.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            session['known'] = False
-            if current_app.config['FLASKY_ADMIN']:
-                send_email(current_app.config['FLASKY_ADMIN'], 'New User',
-                           'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
+    if current_user.can(Permission.WRITE_ARTICLES) and \
+            form.validate_on_submit():
+        post = Post(body=form.body.data,
+                    author=current_user._get_current_object())
+        db.session.add(post)
         return redirect(url_for('.index'))
+    posts = Post.query.order_by(Post.timestamp.desc()).all()
     return render_template('index.html',
                            form=form, name=session.get('name'),
                            known=session.get('known', False),
+                           posts=posts,
                            current_time=datetime.utcnow())
 
 
