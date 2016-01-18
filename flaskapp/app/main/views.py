21eacb2ea0e4d76516d3ec375f83ec9aadc518e6
kasheemlew <417879751@qq.com>
2016-01-18 10:25:09 +0800

    160118

--- flaskapp/app/main/views.py
+++ flaskapp/app/main/views.py
@@ -32,7 +32,8 @@ def user(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
         abort(404)
-    return render_template('user.html', user=user)
+    posts = user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=user, posts=posts)
 
 
 @main.route('/edit-profile', methods=['GET', 'POST'])
