da386509e7f23d638bf06876e789811fc8f8dc12
kasheemlew <417879751@qq.com>
2016-01-22 12:14:48 +0800

    160122

--- flaskapp/config.py
+++ flaskapp/config.py
@@ -14,7 +14,9 @@ class Config:
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flaskapp]'
     FLASKY_MAIL_SENDER = '417879751@qq.com'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
-    FLASKY_POSTS_PER_PAGE = 5 
+    FLASKY_POSTS_PER_PAGE = 5
+    FLASKY_FOLLOWERS_PER_PAGE = 50
+    FLASKY_COMMENTS_PER_PAGE = 30
 
     @staticmethod
     def init_app(app):
