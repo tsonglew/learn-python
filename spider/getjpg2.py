381dd551e431e0206be94160f4d8d09334fc2809
kasheemlew <417879751@qq.com>
2016-03-12 14:30:37 +0800

    160312

---
+++ spider/getjpg2.py
@@ -0,0 +1,17 @@
+import re
+import urllib
+
+
+def getHtml(url):
+    page = urllib.urlopen(url)
+    html = page.read()
+    return html
+
+def getImg(html):
+    reg = r'src="(.+?\.jpg)" pic_ext'
+    imgre = re.compile(reg)
+    imglist = re.findall(imgre, html)
+    return imglist
+
+html = getHtml("http://tieba.baidu.com/p/2460150866")
+print getImg(html)
