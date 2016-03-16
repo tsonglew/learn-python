932a5aebe567d888a64cb53ccffd83d6a1cea397
kasheemlew <417879751@qq.com>
2016-03-16 19:02:28 +0800

    160316

--- scrial/scrial/spiders/dmoz_spider.py
+++ scrial/scrial/spiders/dmoz_spider.py
@@ -10,6 +10,8 @@ class DmozSpider(scrapy.spiders.Spider):
             ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2]
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+        for sel in response.xpath('//ul/li'):
+            title = sel.xpath('a/text()').extract()
+            link = sel.xpath('a/@href').extract()
+            desc = sel.xpath('text()').extract()
+            print title, link, desc
