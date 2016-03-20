a019231ea0ba586a2b74a8daff414db40a1f4b14
kasheemlew <417879751@qq.com>
2016-03-20 15:42:07 +0800

    160320

--- tornado/Burt/main.py
+++ tornado/Burt/main.py
@@ -10,22 +10,6 @@ from tornado.options import options, define
 define("port", default=8000, help="run on the given port", type=int)
 
 
-# 定义Application子类
-class Application(tornado.web.Application):
-    def __init__(self):
-        # 创建处理类列表
-        handlers = [
-                ('/', MainHandler)
-                ]
-        # 设置字典
-        settings = dict(
-                template_path=os.path.join(os.path.dirname(__file__), "templates"),
-                static_path=os.path.join(os.path.dirname(__file__), "static"),
-                debug=True
-                )
-        tornado.web.Application.__init__(self, handlers, **settings)
-
-
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render(
@@ -35,8 +19,42 @@ class MainHandler(tornado.web.RequestHandler):
                 )
 
 
+class RecommendedHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render(
+                "recommended.html",
+                page_title="Burt's Books | Recommended Reading",
+                header_text="Recommeded Reading",
+                books=[
+                    {
+                        "title": "Programming",
+                        "subtitle": "Building",
+                        "image": "/static/images/1.gif",
+                        "author": "Toby",
+                        "date_added": 131,
+                        "date_released": "August 2007",
+                        "isbn": "978-0-596",
+                        "description": "<p>This fascinationg book </p>"
+                    }
+                ]
+        )
+
+
+class BookModule(tornado.web.UIModule):
+    def render(self, book):
+        return self.render_string('modules/book.html', book=book)
+
+
 if __name__ == "__main__":
     tornado.options.parse_command_line()
-    http_server = tornado.httpserver.HTTPServer(Application())
+    app = tornado.web.Application(
+            handlers=[(r'/', MainHandler),
+                (r'/recommended/', RecommendedHandler)
+            ],
+            template_path=os.path.join(os.path.dirname(__file__), 'templates'),
+            static_path=os.path.join(os.path.dirname(__file__), 'static'),
+            ui_modules={'Book': BookModule}
+    )
+    http_server = tornado.httpserver.HTTPServer(app)
     http_server.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
