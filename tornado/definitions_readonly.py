39c0f43999bd59c581b7ea643c5523a3e4459824
kasheemlew <417879751@qq.com>
2016-03-21 21:04:25 +0800

    160321

--- tornado/definitions_readonly.py
+++ tornado/definitions_readonly.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -13,16 +14,20 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [(r"/(\w+)", WordHandler)]
         conn = pymongo.MongoClient("localhost", 27017)
+        # 在Application对象中添加db属性,以在任何RequestHandler对象中使用self.application.db访问
         self.db = conn["example"]
         tornado.web.Application.__init__(self, handlers, debug=True)
 
 
 class WordHandler(tornado.web.RequestHandler):
     def get(self, word):
+        #  将集合对象指定给变量coll
         coll = self.application.db.words
         word_doc = coll.find_one({"word": word})
         if word_doc:
+            # 删除_id键(以便Python的json库将其序列化)
             del word_doc["_id"]
+            # write方法自动序列化字典为JSON格式
             self.write(word_doc)
         else:
             self.set_status(404)
