1000fb4fb27495814d898adaa098b5a8d613cb77
kasheemlew <417879751@qq.com>
2016-03-23 19:26:36 +0800

    160323

--- tornado/tweet/tweet_rate.py
+++ tornado/tweet/tweet_rate.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -17,10 +18,16 @@ define("port", default=8000, help="run on the given port", type=int)
 
 class IndexHandler(tornado.web.RequestHandler):
     def get(self):
+        # 从查询参数中抓取参数q
         query = self.get_argument('q')
+        # 用q执行一个到twitter搜索API的请求
+        # 实例化一个Tornado的HTTPClient类
         client = tornado.httpclient.HTTPClient()
+        # 调用fetch方法(使用要获取的url作参数),返回一个HTTPResponse对象,body包含获取的任何数据(json格式)
         response = client.fetch("http://search.twitter.com/search.json?" + \
+                # 将url中的键值对以链接符&划分
                 urllib.urlencode({"q": query, "result_type": "recent", "rpp": 100}))
+        # 使用json模块来从结果中创建一个数据结构
         body = json.loads(response.body)
         result_count = len(body['results'])
         now = datetime.datetime.utcnow()
