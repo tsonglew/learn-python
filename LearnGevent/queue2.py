ab9e9df194801d2bc14f52ec7f014af34b331af2
kasheemlew <417879751@qq.com>
2016-04-24 12:52:54 +0800

    update gevent

---
+++ LearnGevent/queue2.py
@@ -0,0 +1,34 @@
+import gevent
+from gevent.queue import Queue, Empty
+
+tasks = Queue(maxsize=3)
+
+def worker(n):
+    try:
+        while True:
+            task = tasks.get(timeout=1) # decrements queue size by 1
+            print('Worker %s got task %s' % (n, task))
+            gevent.sleep(0)
+    except Empty:
+        print('Quitting time!')
+
+def boss():
+    """
+    Boss will wait to hand out work until a individual worker is
+    free since the maxsize of the task queue is 3.
+    """
+
+    for i in xrange(1,10):
+        tasks.put(i)
+    print('Assigned all work in iteration 1')
+
+    for i in xrange(10,20):
+        tasks.put(i)
+    print('Assigned all work in iteration 2')
+
+gevent.joinall([
+    gevent.spawn(boss),
+    gevent.spawn(worker, 'steve'),
+    gevent.spawn(worker, 'john'),
+    gevent.spawn(worker, 'bob'),
+])
