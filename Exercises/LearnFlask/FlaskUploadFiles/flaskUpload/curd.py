7e1e42bbc345943de06b55b2d2ebcd7fcfd161d7
kasheemlew <417879751@qq.com>
2016-10-09 15:25:11 +0800

    updated

--- Exercises/LearnFlask/FlaskUploadFiles/flaskUpload/curd.py
+++ Exercises/LearnFlask/FlaskUploadFiles/flaskUpload/curd.py
@@ -4,4 +4,22 @@ from consts import HOSTNAME, DATABASE, USERNAME, PASSWORD
 con = MySQLdb.connect(HOSTNAME, USERNAME, PASSWORD, DATABASE)
 
 with con as cur:
-    cur.execute
+    cur.execute('drop table if exists users')
+    cur.execute('create table users(Id INT PRIMARY KEY AUTO_INCREMENT, '
+            'Name VARCHAR(25))')
+    cur.execute("insert into users(Name) values('kasheemlew')")
+    cur.execute("insert into users(Name) values('admin')")
+    cur.execute('select * from users')
+
+    rows = cur.fetchall()
+    for row in rows:
+        print row
+    cur.execute('update users set Name=%s where Id=%s', ('kasheem', 1))
+    print 'Number of rows updated:', cur.rowcount
+
+    cur = con.cursor(MySQLdb.cursors.DictCursor)
+    cur.execute('select * from users')
+
+    rows = cur.fetchall()
+    for row in rows:
+        print row['Id'], row['Name']
